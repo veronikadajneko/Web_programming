81f638a9f89d5ca8b99bfc533ef0f50dcccc7f28
Veronika <dajneko01@inbox.ru>
2020-05-12 20:54:21 +0300

    task6

--- script_js/script.js
+++ script_js/script.js
@@ -6,7 +6,7 @@
             description: 'как же хорошо, что Вероника наконец делает вёрстку',
             createdAt: new Date('2020-05-05T15:31:00'),
             author: 'ar_kud',
-            photoLink: 'pictures/tweetphoto/cheer.jpg',
+            photoLink: 'tweetphoto/cheer.jpg',
             hashTags: ['фронтендерка'],
             likes: ['ar_kud', 'veronikchaa']
         },
@@ -16,7 +16,7 @@
             description: 'меняю Пашу на Серёжу',
             createdAt: new Date('2020-01-16T03:20:00'),
             author: 'eraztuna',
-            photoLink: 'pictures/tweetphoto/love.jpg',
+            photoLink: 'tweetphoto/love.jpg',
             hashTags: ['boy', 'problems'],
             likes: ['veronikchaa', 'ar_kud', 'maxoshka', 'sstrazdinaa']
         },
@@ -26,7 +26,7 @@
             description: 'Понимаю',
             createdAt: new Date('2020-04-05T15:17:00'),
             author: 'maxoshka',
-            photoLink: 'pictures/tweetpost/understand.jpg',
+            photoLink: 'tweetpost/understand.jpg',
             hashTags: ['жиза'],
             likes: ['eraztuna', 'veronikchaa']
         },
@@ -36,7 +36,7 @@
             description: 'купи сладкое что-нибудь,тебе нужно.\n'+ 'Просто если с головой какие-то проблемы, то нужен сахар',
             createdAt: new Date('2019-12-14T12:11:25'),
             author: 'ar_kud',
-            photoLink: 'pictures/tweetpost/needsweet.jpg',
+            photoLink: 'tweetpost/needsweet.jpg',
             hashTags: ['sweet', 'sugar'],
             likes: ['sstrazdinaa', 'pvolchetskaya', 'eraztuna' , 'veronikchaa']
         },
@@ -46,7 +46,7 @@
             description: 'шаурма окутана любовью, потому что она завёрнута в loveаш',
             createdAt: new Date('2019-11-12T18:33:00'),
             author: 'VslvaD',
-            photoLink: 'pictures/tweetpost/loveашь.jpg',
+            photoLink: 'tweetpost/loveашь.jpg',
             hashTags: ['fun', 'love', 'food'],
             likes: ['eraztuna', 'veronikchaa', 'ar_kud', 'sstrazdinaa']
         },
@@ -56,7 +56,7 @@
             description: 'вот бы я тоже так тянулась к знаниям, как мой кот к оливкам',
             createdAt: new Date('2019-12-18T23:29:10'),
             author: 'sstrazdinaa',
-            photoLink: 'pictures/tweetpost/cat.jpg',
+            photoLink: 'tweetpost/cat.jpg',
             hashTags: ['котики', 'универ', 'оливки'],
             likes: ['veronikchaa', 'maxoshka', 'ar_kud', 'eraztuna']
         },
@@ -66,7 +66,7 @@
             description: 'сегодня взяла неберущийся интеграл',
             createdAt: new Date('2019-10-20T09:00:03'),
             author: 'trofima',
-            photoLink: 'pictures/tweetpost/integral.jpg',
+            photoLink: 'tweetpost/integral.jpg',
             hashTags: ['математик', 'famcslove'],
             likes: ['sstrazdinaa', 'pavel.tyl']
         },
@@ -86,7 +86,7 @@
             description: 'так,для себя нужно записать\n' + 'где право,а где лево,а то я уже путаю',
             createdAt: new Date('2019-09-30T21:07:04'),
             author: 'veronikchaa',
-            photoLink: 'pictures/tweetpost/leftright.png',
+            photoLink: 'tweetpost/leftright.png',
             hashTags: ['какобычно'],
             likes: ['eraztuna', 'maxoshka']
         },
@@ -96,7 +96,7 @@
             description: 'каждый год около 200 человек умирает от укусов диких муравьев',
             createdAt: new Date('2020-04-25T12:23:08'),
             author: 'la_pank_mne',
-            photoLink: 'pictures/goldrain.jpg',
+            photoLink: 'goldrain.jpg',
             hashTags: ['#ужас', 'страшно'],
             likes: ['eraztuna', 'veronikchaa']
         },
@@ -106,7 +106,7 @@
             description: 'я хочу быть красивой, а не в шапке',
             createdAt: new Date('2019-09-21T11:26:07'),
             author: 'eraztuna',
-            photoLink: 'pictures/tweetpost/mashka.jpg',
+            photoLink: 'tweetpost/mashka.jpg',
             hashTags: ['beautiful', 'girls'],
             likes: ['veronikchaa', 'ar_kud', 'pavel.tyl']
         },
@@ -116,7 +116,7 @@
             description: 'вероника пользуется мужиками',
             createdAt: new Date('2020-04-03T01:29:00'),
             author: 'ar_kud',
-            photoLink: 'pictures/tweetpost/artemka.jpg',
+            photoLink: 'tweetpost/artemka.jpg',
             hashTags: ['justfriends'],
             likes: ['maxoshka']
         },
@@ -126,7 +126,7 @@
             description: 'справедливо',
             createdAt: new Date('2020-02-20T02:20:00'),
             author: 'maxoshka',
-            photoLink: 'pictures/tweetpost/understand.jpg',
+            photoLink: 'tweetpost/understand.jpg',
             hashTags: ['жиза'],
             likes: ['ar_kud']
         },
@@ -136,7 +136,7 @@
             description: 'кто бы мог подумать, что "Поле Чудес" такая весёлая телепереача',
             createdAt: new Date('2019-03-27T19:33:00'),
             author: 'pvolchetskaya',
-            photoLink: 'pictures/tweetpost/полечудес.jpg',
+            photoLink: 'tweetpost/полечудес.jpg',
             hashTags: ['развлечения', 'самоизаляция'],
             likes: ['veronikchaa', 'sstrazdinaa', 'pvolchetskaya', 'eraztuna', 'ar_kud', 'trofima']
         },
@@ -146,7 +146,7 @@
             description: 'петровщина не греет',
             createdAt: new Date('2019-12-05T21:10:00'),
             author: 'eraztuna',
-            photoLink: 'pictures/tweetpost/petrovshina.jpg',
+            photoLink: 'tweetpost/petrovshina.jpg',
             hashTags: ['metro', 'winter'],
             likes: ['veronikchaa', 'trofima']
         },
@@ -156,7 +156,7 @@
             description: 'счастье в горах',
             createdAt: new Date('2015-10-20T09:12:03'),
             author: 'la_pank_mne',
-            photoLink: 'pictures/tweetpost/mountain.jpg',
+            photoLink: 'tweetpost/mountain.jpg',
             hashTags: ['nature', 'happy'],
             likes: ['la_pank_mne', 'veronikchaa']
         },
@@ -166,7 +166,7 @@
             description: 'лучше сделать и пожалеть, чем не сделать и пожалеть',
             createdAt: new Date('2018-02-20T21:10:00'),
             author: 'ghost_of_lack',
-            photoLink: 'pictures/tweetpost/ola.jpg',
+            photoLink: 'tweetpost/ola.jpg',
             hashTags: ['life'],
             likes: ['la_pank_mne', 'veronikchaa']
         },
@@ -176,7 +176,7 @@
             description: 'переделай, мне не нравится',
             createdAt: new Date('2019-10-05T012:10:00'),
             author: 'pavel.tyl',
-            photoLink: 'pictures/tweetpost/code.jpg',
+            photoLink: 'tweetpost/code.jpg',
             hashTags: ['programming'],
             likes: ['veronikchaa', 'eraztuna']
         },
@@ -186,7 +186,7 @@
             description: 'Вероника - солнышко',
             createdAt: new Date('2020-05-12T01:05:26'),
             author: 'maxoshka',
-            photoLink: 'pictures/tweetpost/sun.jpg',
+            photoLink: 'tweetpost/sun.jpg',
             hashTags: ['sun'],
             likes: ['eraztuna', 'veronikchaa']
         },
@@ -196,7 +196,7 @@
             description: 'использую два if через main',
             createdAt: new Date('2019-07-20T18:00:03'),
             author: 'trofima',
-            photoLink: 'pictures/tweetpost/prog.jpg',
+            photoLink: 'tweetpost/prog.jpg',
             hashTags: ['proga'],
             likes: ['veronikchaa', 'ar_kud']
         }
@@ -213,6 +213,11 @@
 
         function getPosts(skip = 0, top = 0, filterConfig = undefined) {
 
+        if (typeof skip !== 'number' || typeof top !== 'number') {
+            console.log('Incorrect type');
+            return;
+        }
+
         let result = posts;
 
         if (filterConfig) {
@@ -224,10 +229,10 @@
                 });
             }
 
-            if (filterConfig.dateSort) {
+            else if (filterConfig.dateSort) {
                 result = result.sort(compareDates);
             } 
-            if (filterConfig.author) {
+            else if (filterConfig.author) {
                 result = result.filter(function (post) {
                     if (post.author.includes(filterConfig.author))
                         return post.author;
@@ -293,7 +298,7 @@
 
     //tests
 
-    console.log("Top 5 posts:");
+    console.log("Top 6 posts:");
     console.log(getPosts(0,6));
 
     console.log("Top 20 posts, skip 0 post, author: ar_kud:");
@@ -310,6 +315,13 @@
 
     console.log("Add invalid post:");
     console.log(addPost({description: ""}));
+
+    console.log("Edit added post:");
+    console.log(editPost('10',{description: "changed"}));
+    console.log(getPost('10'));
+
+    console.log("Delete edited post:");
+    console.log(removePost('10'));
 
 }());
 
